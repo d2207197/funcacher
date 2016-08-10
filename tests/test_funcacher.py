b45aa35222718bc1653a60edee6230eeac230a0e
顏孜羲 <>
2016-08-11 00:36:53 +0800

    test case

--- tests/test_funcacher.py
+++ tests/test_funcacher.py
@@ -1,14 +1,23 @@
 import pytest
 from funcacher import FunCacher
 import time
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 pymemcacher = FunCacher()
-@pymemcacher
+
+
+@pymemcacher(b'slow_add')
 def slow_add(a, b):
-    time.sleep(1)
+    time.sleep(2)
     return a + b
 
 
-@pytest.mark.timeout(500)
-def test_pymemcacher():
-    slow_add(2, 3)
+@pytest.fixture
+def cache_slow_add():
+    slow_add(1, 3)
+
+
+@pytest.mark.timeout(3)
+def test_pymemcacher(cache_slow_add):
+    print(cache_slow_add)
+    slow_add(1, 3)
